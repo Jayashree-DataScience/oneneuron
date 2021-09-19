877378b181d5dbc60569c3a4a4332d5001224396
JayashreeHV <jayashreehv7879@gmail.com>
2021-09-19 17:14:41 +0530

    log created

--- and.py
+++ and.py
@@ -1,16 +1,46 @@
+
+
 from utils.model import Perceptron
 from utils.all_utils import prepare_data
-AND={
-    "x1":[0,0,1,1],
-    "x2":[0,1,0,1],
-    "y":[0,0,0,1]
-    
-    }
-df=pd.DataFrame(AND)
-x,y=prepare_data(df)
-ETA=0.3
-EPOCHS=10
+import pandas as pd
+import numpy as np
+from utils.model import Perceptron
+from utils.all_utils import prepare_data, save_plot, save_model
+import pandas as pd
+import logging
+import os
 
-model=Perceptron(eta=ETA, epochs=EPOCHS)
-model.fit(x,y)
-_=model.total_loss()
+logging_str = "[%(asctime)s: %(levelname)s: %(module)s] %(message)s"
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+logging.basicConfig(filename= os.path.join(log_dir,"running_logs.log"),level=logging.INFO, format=logging_str, filemode="a")
+
+
+
+
+def main(data, modelName, plotName, eta, epochs):
+    df = pd.DataFrame(data)
+    #print(df)
+    logging.info(f"This is actual dataframe{df}")
+    X, y = prepare_data(df)
+    model = Perceptron(eta=eta, epochs=epochs)
+    model.fit(X, y)
+    _ = model.total_loss()
+    save_model(model, filename=modelName)
+    save_plot(df, plotName, model)
+
+if __name__ == '__main__':
+    AND = {
+        "x1": [0,0,1,1],
+        "x2": [0,1,0,1],
+        "y": [0,0,0,1],
+    }
+    ETA = 0.3 # 0 and 1
+    EPOCHS = 10
+    try:
+        logging.info(">>>>>>>>>>>>STARTING TRAINING>>>>>>>>>")
+        main(data=AND, modelName="and.model", plotName="and.png", eta=ETA, epochs=EPOCHS)
+        logging.info("<<<<<<<TRAINING DONE SUCCESSFULLY<<<<<<<<")
+    except Exception as e:
+        logging.exception(e)
+        raise e
--- utils/model.py
+++ utils/model.py
@@ -1,41 +1,49 @@
+import numpy as np
+import logging
+from tqdm import tqdm
+import os
+
+
+
+
 class Perceptron:
-    def __init__(self,eta,epochs):
-        self.weights=np.random.randn(3)*1e-4
-        print(f"Initial weights before training: {self.weights}")
-        self.eta=eta
-        self.epochs=epochs
+  def __init__(self, eta, epochs):
+    self.weights = np.random.randn(3) * 1e-4 # SMALL WEIGHT INIT
+    logging.info(f"initial weights before training: \n{self.weights}")
+    self.eta = eta # LEARNING RATE
+    self.epochs = epochs 
 
-    def activateFunction(self,inputs,weights):
-        z=np.dot(input,weights)
-        return np.where(z>0,1,0)
 
-    def fit(self,x,y):
-        self.x=x
-        self.y=y
-        x_with_bias=np.c_[self.x,-np.ones((len(self.x),1))]
-        print(f"X with bias:{x_with_bias}")
-        for epoch in range(self.epochs):
-            print("--"*10)
-            print(f"for epoch:{epoch}")
-            print("--"*10)
-            y_hat=self.activationFunction(X_with_bias,self.weights)
-            print(f"predicted value after forward propagation:{y_hat}")
-            self.weights=self.weights+self.eta*np.dot(x_with_bias.T,self.error)
-            print(f"error:{self.error}")
-            print("updated weights after epoch:{epoch}/{self.epochs}: {self.weights")
-            print("######"*10)
-    
-    def predict(self,x):
-        x_with_bias=np.c_[self.x,-np.ones((len(x),1))]
-        return self.activateFunction(x_with_bias,self.weights)
-    
-    def total_loss(self):
-        total_loss=np.sum(self.error)
-        print(f"total loss:{total_loss}")
-        return total_loss
+  def activationFunction(self, inputs, weights):
+    z = np.dot(inputs, weights) # z = W * X
+    return np.where(z > 0, 1, 0) # CONDITION, IF TRUE, ELSE
 
-    def prepare_data(df):
-        x=df.drop("y",axis=1)
-        y=df["y"]
-        return x,y
+  def fit(self, X, y):
+    self.X = X
+    self.y = y
 
+    X_with_bias = np.c_[self.X, -np.ones((len(self.X), 1))] # CONCATINATION
+    logging.info(f"X with bias: \n{X_with_bias}")
+
+    for epoch in tqdm(range(self.epochs),total=self.epochs,desc="training the model"):
+      logging.info("--"*10)
+      logging.info(f"for epoch: {epoch}")
+      logging.info("--"*10)
+
+      y_hat = self.activationFunction(X_with_bias, self.weights) # foward propagation
+      logging.info(f"predicted value after forward pass: \n{y_hat}")
+      self.error = self.y - y_hat
+      logging.info(f"error: \n{self.error}")
+      self.weights = self.weights + self.eta * np.dot(X_with_bias.T, self.error) # backward propagation
+      logging.info(f"updated weights after epoch:\n{epoch}/{self.epochs} : \n{self.weights}")
+      logging.info("#####"*10)
+
+
+  def predict(self, X):
+    X_with_bias = np.c_[X, -np.ones((len(X), 1))]
+    return self.activationFunction(X_with_bias, self.weights)
+
+  def total_loss(self):
+    total_loss = np.sum(self.error)
+    logging.info(f"total loss: {total_loss}")
+    return total_loss
